e28c5c2b7cd07a746623409ed2b7d206469961df
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-03-24 11:13:38 +0700

    menggunakan perulangan for dan menyimpan nilai

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -22,5 +22,11 @@ void input() {               // project input
 	cout << "======================" << endl;     // Membuat tampilan susunan data element array
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
+	for (int i = 0; i < n; i++)                        // Menggunakan perulangan for untuk menyinpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";         // Memasukkan atau menginputkan nilai data n
+		cin >> arr[i];                                 // Menyimpan nilai data n kedalam array arr
 
-	
+	}
+
+}
